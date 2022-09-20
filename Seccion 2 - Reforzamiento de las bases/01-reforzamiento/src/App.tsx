01f98c98db3ca6ed01f063b6abf178117fb0f30d
Daniel Gonzalez <danielgj7@outlook.com>
2022-09-19 23:36:58 -0500

    Inicio de reforzamiento 01

---
+++ Seccion 2 - Reforzamiento de las bases/01-reforzamiento/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { TiposBasicos } from './typescript/TiposBasicos'
+
+const App = () => {
+  return (
+    <div className = "mt-2">
+      <h1>Introducción a TS-React</h1>
+      <hr />
+      <TiposBasicos/>
+    </div>
+  )
+}
+
+export default App
---
+++ Seccion 2 - Reforzamiento de las bases/01-reforzamiento/src/typescript/TiposBasicos.tsx
@@ -0,0 +1,14 @@
+export const TiposBasicos = () => {
+
+    let nombre : string | number = 'Fernando';
+    nombre = 123;
+
+    return (
+        <>
+            <h3>TIpos Basicos</h3>
+            {
+                nombre
+            }
+        </>
+    )
+}
