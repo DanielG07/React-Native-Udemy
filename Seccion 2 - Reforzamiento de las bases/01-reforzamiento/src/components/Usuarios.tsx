d8fead7e9adaea55f80dd6728c64cff642aed43d
Daniel Gonzalez <danielgj7@outlook.com>
2022-09-20 16:49:48 -0500

    Se agrega la lista de los usuarios recuperados del endpoint y se pintan

--- Seccion 2 - Reforzamiento de las bases/01-reforzamiento/src/components/Usuarios.tsx
+++ Seccion 2 - Reforzamiento de las bases/01-reforzamiento/src/components/Usuarios.tsx
@@ -4,18 +4,37 @@ import { ReqResListado, User } from '../interfaces/reqRes';
 
 export const Usuarios = () => {
 
-    const [usuario, setUsuario] = useState<User[]>([])
+    const [usuarios, setUsuarios] = useState<User[]>([])
 
     useEffect(() => {
       // Llamado a API
       reqResApi.get<ReqResListado>('/users')
         .then( resp => {
-            console.log(resp.data.data)
+            setUsuarios( resp.data.data )
         })
         .catch(
             console.log
         )
     }, [])
+
+    const renderItem = ({ id, first_name, last_name, email, avatar}: User) => {
+        return (
+            <tr key={id.toString()}>
+                <td>
+                    <img 
+                        src={ avatar } 
+                        alt= { first_name } 
+                        style={{
+                            width: 50,
+                            borderRadius: 100
+                        }}
+                        />
+                </td>
+                <td>{ first_name } { last_name }</td>
+                <td>{ email }</td>
+            </tr>
+        )
+    }
     
 
     return (
@@ -30,9 +49,16 @@ export const Usuarios = () => {
                     </tr>
                 </thead>
                 <tbody>
-
+                    {
+                        usuarios.map( usuario => renderItem(usuario))
+                    }
                 </tbody>
             </table>
+
+            <button
+                className='btn btn-primary'>
+                    Siguientes
+            </button>
         </>
     )
 }
