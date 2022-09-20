c69fc522b3406009659655ba8b3607f2df064ca7
Daniel Gonzalez <danielgj7@outlook.com>
2022-09-20 11:04:56 -0500

    Objetos literales, como se trabaja con interfaces y establecer moldes

--- Seccion 2 - Reforzamiento de las bases/01-reforzamiento/src/App.tsx
+++ Seccion 2 - Reforzamiento de las bases/01-reforzamiento/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ObjetosLiterales } from './typescript/ObjetosLiterales'
 import { TiposBasicos } from './typescript/TiposBasicos'
 
 const App = () => {
@@ -6,7 +7,8 @@ const App = () => {
     <div className = "mt-2">
       <h1>Introducción a TS-React</h1>
       <hr />
-      <TiposBasicos/>
+      {/* <TiposBasicos/> */}
+      <ObjetosLiterales/>
     </div>
   )
 }
