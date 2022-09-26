073373952941eac53e7b035f66fbec668d67f19b
Daniel González Jiménez <danielgj@MacBook-Pro-de-Daniel.local>
2022-09-25 21:47:32 -0500

    Primera app, usando template de react-native y configuracion del ambiente

---
+++ Seccion-3/AwesomeTSProject/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hola Mundo ya se como lanzarlo sobre un android/n espero que ahora si ande fucnionando muy bien todo esto</Text>
+    </View>
+  )
+}
+
+export default App
