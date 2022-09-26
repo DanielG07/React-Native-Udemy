76ecd134d7c9b488f91a9fc89463992caadb64f8
Daniel González Jiménez <danielgj@MacBook-Pro-de-Daniel.local>
2022-09-25 23:04:25 -0500

    Se hace un fab con el TouchabelOpacity, para incrementar y decrementar el contador

--- 02-MiPrimeraApp/src/screens/CounterScreen.tsx
+++ 02-MiPrimeraApp/src/screens/CounterScreen.tsx
@@ -10,16 +10,26 @@ export const CounterScreen = () => {
             <Text style={ styles.title }>Contador: {contador}</Text>
 
             <TouchableOpacity
-                onPress={() => setContador( contador + 1 )} 
+                onPress={() => setContador( contador - 1 )} 
+                style={ styles.fabLocationBL }
             >
-                <View style={ styles.buttonIncrement }>
-                    <Text>Click me!!!</Text>
+                <View style={ styles.fab }>
+                    <Text style={ styles.fabText }>-1</Text>
+                </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                onPress={() => setContador( contador + 1 )} 
+                style={ styles.fabLocationBR }
+            >
+                <View style={ styles.fab }>
+                    <Text style={ styles.fabText }>+1</Text>
                 </View>
             </TouchableOpacity>
         </View>
     )
 }
-
+ 
 const styles = StyleSheet.create ({
     container: {
         flex: 1,
@@ -29,8 +39,27 @@ const styles = StyleSheet.create ({
         textAlign: 'center',
         fontSize: 30
     },
-    buttonIncrement: {
-        backgroundColor:  'red',
-        borderRadius: 100
+    fabLocationBR: {
+        position: 'absolute',
+        bottom: 16,
+        right: 16
+    },
+    fabLocationBL: {
+        position: 'absolute',
+        bottom: 16,
+        left: 16
+    },
+    fab: {
+        backgroundColor: '#5856D6',
+        width: 60,
+        height: 60,
+        borderRadius: 100,
+        justifyContent: 'center'
+    },
+    fabText: {
+        color:'white',
+        fontSize: 25,
+        fontWeight: 'bold',
+        alignSelf: 'center'
     }
 })
