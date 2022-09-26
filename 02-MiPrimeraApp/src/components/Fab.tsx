c61b559111e0f8724d0506e072f8f7f1d6d5f17f
Daniel González Jiménez <danielgj@MacBook-Pro-de-Daniel.local>
2022-09-26 15:17:30 -0500

    Se hace un cambio del TouchableOpacity por el TouchableNativeFeedback, adicionandole una efecto en android al hacerle click

--- 02-MiPrimeraApp/src/components/Fab.tsx
+++ 02-MiPrimeraApp/src/components/Fab.tsx
@@ -1,34 +1,38 @@
 import React from 'react'
-import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, View, Text, StyleSheet, TouchableNativeFeedback } from 'react-native';
 
 interface Props {
     title: string,
     position?: 'br' | 'bl',
     onPress: () => void, 
 }
-
+ 
 export const Fab = ({ title, onPress, position = 'br' }: Props) => {
   return (
-    <TouchableOpacity 
-        onPress={ onPress  } 
-        style={ styles.fabLocationBR }
-        >
+    <View 
+        style={[ styles.fabLocation, 
+            ( position  === 'bl' ) ?  styles.left : styles.right
+        ]}>
+    <TouchableNativeFeedback 
+        onPress={ onPress }
+        background={ TouchableNativeFeedback.Ripple('black', false, 30) }>
             <View style={ styles.fab }>
                 <Text style={ styles.fabText }>{ title }</Text>
             </View>
-    </TouchableOpacity>
+    </TouchableNativeFeedback>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-    fabLocationBR: {
+    fabLocation: {
         position: 'absolute',
-        bottom: 16,
+        bottom: 16
+    },
+    right: {
         right: 16
     },
-    fabLocationBL: {
-        position: 'absolute',
-        bottom: 16,
+    left: {
         left: 16
     },
     fab: {
@@ -36,7 +40,16 @@ const styles = StyleSheet.create({
         width: 60,
         height: 60,
         borderRadius: 100,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+
+        elevation: 8
     },
     fabText: {
         color:'white',
