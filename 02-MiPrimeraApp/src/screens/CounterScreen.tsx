1888f3876b599aecc842cc7b050ffa61e4883871
Daniel González Jiménez <danielgj@MacBook-Pro-de-Daniel.local>
2022-09-25 22:44:58 -0500

    Se separan los styles de los componentes y se colocan en un StyleSheet

--- 02-MiPrimeraApp/src/screens/CounterScreen.tsx
+++ 02-MiPrimeraApp/src/screens/CounterScreen.tsx
@@ -1,30 +1,36 @@
 import React, { useState } from 'react'
-import { Button, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 export const CounterScreen = () => {
 
     const [contador, setContador] = useState(10)
 
     return (
-        <View style={{
-            flex: 1,
-            justifyContent: 'center'
-        }}>
-            <Text style={{
-                textAlign: 'center',
-                fontSize: 30,
-            }}>Contador: {contador}</Text>
+        <View style={ styles.container }>
+            <Text style={ styles.title }>Contador: {contador}</Text>
 
             <TouchableOpacity
                 onPress={() => setContador( contador + 1 )} 
             >
-                <View style={{
-                    backgroundColor: 'red',
-                    borderRadius: 100
-                }}>
+                <View style={ styles.buttonIncrement }>
                     <Text>Click me!!!</Text>
                 </View>
             </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create ({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 30
+    },
+    buttonIncrement: {
+        backgroundColor:  'red',
+        borderRadius: 100
+    }
+})
