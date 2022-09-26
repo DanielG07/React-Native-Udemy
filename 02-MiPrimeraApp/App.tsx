65c291798ed6b6e8dc0f0a9a6eadfb0e70527a03
Daniel González Jiménez <danielgj@MacBook-Pro-de-Daniel.local>
2022-09-25 22:29:50 -0500

    Se crea un contador simple que se ve en pantalla

--- 02-MiPrimeraApp/App.tsx
+++ 02-MiPrimeraApp/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Text, View } from 'react-native';
+import { CounterScreen } from './src/screens/CounterScreen';
 import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 
 const App = () => {
   return (
-    <HolaMundoScreen/>
+    //<HolaMundoScreen/>
+    <CounterScreen/>
   )
 }
 
---
+++ 02-MiPrimeraApp/src/screens/CounterScreen.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import { Button, Text, View } from 'react-native'
+
+export const CounterScreen = () => {
+
+    const [contador, setContador] = useState(10)
+
+    return (
+        <View style={{
+            flex: 1,
+            justifyContent: 'center'
+        }}>
+            <Text style={{
+                textAlign: 'center',
+                fontSize: 30,
+            }}>Contador: {contador}</Text>
+
+            <Button
+                title='Click'
+                onPress={ () => setContador( contador + 1 )}
+            />
+        </View>
+    )
+}
