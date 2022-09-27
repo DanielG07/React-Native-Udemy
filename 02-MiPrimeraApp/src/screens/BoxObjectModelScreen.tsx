d295c0849d35687a1b335e8baa202f972c510d87
Daniel González Jiménez <danielgj@MacBook-Pro-de-Daniel.local>
2022-09-27 12:45:48 -0500

    Se hacen pruebas con el Box Object Model

--- 02-MiPrimeraApp/src/screens/BoxObjectModelScreen.tsx
+++ 02-MiPrimeraApp/src/screens/BoxObjectModelScreen.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 export const BoxObjectModelScreen = () => {
   return (
-    <View>
-        <Text>Box Object Model</Text>
+    <View
+        style={ styles.container }
+    >
+        <Text
+            style={ styles.title }
+        >Box Object Model</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'red'
+    },
+    title: {
+        fontSize: 30,
+        width: 150,
+        borderWidth: 10
+    }
+})
